98d4da5a771ba69f83e410bc77e0775da1bcd75c
bob-603 <126084505+bob-603@users.noreply.github.com>
2023-02-23 11:38:45 +0200

    Language
    Language

---
+++ resources/lang/pl/messages.php
@@ -0,0 +1,10 @@
+<?php
+return [
+	'Language' => 'Język',
+	'Login' => 'Zaloguj',
+	'Register' => 'Zarejestruj się',
+	'Email Address' => 'Adres e-mail',
+	'Password' => 'Hasło',
+	'Remember Me' => 'Zapamiętaj mnie',
+	'Forgot Your Password?' => 'Zapomniałeś hasła?',
+];
