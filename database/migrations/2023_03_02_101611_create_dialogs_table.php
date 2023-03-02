c44672212e644b1006b9f4a94f6ae037cbb46ac4
Ramedlav <ochev.vladimir@gmail.com>
2023-03-02 11:22:52 +0100

    migrations

---
+++ database/migrations/2023_03_02_101611_create_dialogs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDialogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dialogs', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dialogs');
+    }
+}
