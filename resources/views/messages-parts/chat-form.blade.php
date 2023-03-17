120d6320199b7ab493fd50c22d939f68d4772b4e
Vitaliy <vinester72@gmail.com>
2023-03-17 10:08:05 +0200

    messages page d

---
+++ resources/views/messages-parts/chat-form.blade.php
@@ -0,0 +1,84 @@
+<div class=" css-15ck3pw">
+    <div class="css-ofyax4">
+        <button class="css-25032o">
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 24 24"
+                 width="1em"
+                 height="1em"
+                 class="css-9ct399">
+                 <path fill="currentColor" fill-rule="evenodd" d="M2.001 6.5h1.414l1.27 1.271 7.316 7.315 7.315-7.315L20.587 6.5h1.414v1.414l-1.27 1.27-7.316 7.316-1 1h-.827l-3.942-3.942-4.374-4.374-1.27-1.27z"></path>
+            </svg>
+        </button>
+    </div>
+    <div  class="css-t0lbh8">
+        <form class="css-1tqh4ox">
+            <div class="css-ygggrs">
+                <div class="css-ocljxl">
+                    <div class="css-g4yxqq">
+                        <fieldset aria-hidden="false" class="css-n8ph6a">
+                            <div class="css-1povu0j">
+                                <label class="css-1nvys81" for="image-upload">
+                                    <span title="{{__('attach a photo')}}">
+                                        <div class="css-1i2rxeu">
+                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                 viewBox="0 0 24 24"
+                                                 style="height: 24px; width: 24px;">
+                                                 <path d="M4.513 4L3.236 5.07v14.835L4.29 21h14.932l1.015-1.095V5.226L19.11 4H4.513zM7.25 7.081a.638.638 0 100 1.277.638.638 0 000-1.277zm0 .478a.16.16 0 01.16.16.16.16 0 01-.16.16.16.16 0 01-.16-.16.16.16 0 01.16-.16zM3 17.564L7.084 12.5l4.416 2.833 3.637-4.604L20 15.333" stroke="#002F34" stroke-width="2" fill="none" fill-rule="evenodd"></path>
+                                            </svg>
+                                        </div>
+                                    </span>
+                                    <input type="file"
+                                           id="image-upload"
+                                           multiple=""
+                                           accept=".jpg, .jpeg, .png"
+                                           class="css-v3axa2">
+                                </label>
+                                <label class="css-1nvys81" for="documents-upload">
+                                    <span title="{{__('attach a file')}}">
+                                        <div class="css-1i2rxeu">
+                                            <svg width="24px"
+                                                 height="24px"
+                                                 viewBox="0 0 24 24"
+                                                 xmlns="http://www.w3.org/2000/svg"
+                                                 class="css-pyu9k9">
+                                                 <path fill="currentColor" d="M12.597 2 2 12.945v7.578L3.285 22h7.842L22 10.77V8.772l-4.777-4.936h-1.934l-9.389 9.7v4.31h4.828l6.132-6.333-1.406-1.452-5.55 5.734H7.885v-1.41l8.371-8.644 3.901 4.03-9.853 10.177H3.987v-6.153L14 3.45z" fill-rule="evenodd"></path>
+                                            </svg>
+                                        </div>
+                                    </span>
+                                    <input type="file"
+                                           id="documents-upload"
+                                           multiple=""
+                                           accept=".jpg, .jpeg, .png"
+                                           class="css-v3axa2">
+                                </label>
+                            </div>
+                        </fieldset>
+                    </div>
+                    <textarea rows="1"
+                              name="message.text"
+                              placeholder="{{__('Write a notification...')}}"
+                              class="css-1kjjac5"
+                              style="margin-right: -16px;"></textarea>
+                    <div class="css-qxaskb">
+                        <button aria-hidden="true"
+                                disabled=""
+                                aria-label="Submit message"
+                                type="submit"
+                                {{-- class="css-2zsnaw" --}}
+                                class="css-9x8rrm"
+                                style="cursor: pointer;">
+                                <svg width="1em"
+                                     height="1em"
+                                     viewBox="0 0 24 24"
+                                     xmlns="http://www.w3.org/2000/svg"
+                                     aria-hidden="true"
+                                     class="css-15wjrqi">
+                                     <path d="m2 3 3.287 8.998L2 20.995l20-8.997L2 3zm3.61 3.883 10.225 4.6H7.292l-1.682-4.6zm1.682 5.63h8.543l-10.225 4.6 1.682-4.6z" fill="currentColor" fill-rule="evenodd"></path>
+                                 </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+<div>
